b266f38207487dec9abff4144f03e2eba4ce6dcb
Jakub Malec <jakub.malec@outlook.com>
2019-05-19 10:19:39 +0200

    ver0.0 added license

---
+++ soundsphere/app.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+__author__ = "Jakub Malec"
+__copyright__ = "Copyright 2019, Jakub Malec"
+__credits__ = ["Jakub Malec"]
+
+__license__ = "MIT"
+__version__ = "0.0"
+__maintainer__ = "Jakub Malec"
+__email__ = "jakub.malec@outlook.com"
+__status__ = "Dev"
