d195156dd44247f624f1482d54a2cab377e4c964
Jakub Malec <jakub.malec@outlook.com>
2019-05-27 19:53:32 +0200

    ver0.2 - installation possible

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 echo Adding autostart...
 sudo mv -f /usr/local/bin/SoundSphere/soundsphere.desktop /etc/xdg/autostart
+
 echo Updating repository list...
 sudo apt-get update -yqq
+
 echo Updating existing software...
 sudo apt-get upgrade -yqq
+
 echo Installing Chromium...
 sudo apt-get install chromium-browser -yqq
+
 echo Installing Python interpreter...
 sudo apt-get install python2.7 -yqq
+
 echo Installing additional software...
 sudo apt-get install python-tk -yqq
+
+for i in 10 9 8 7 6 5 4 3 2 1
+do
+        echo "Installation completed. System reboot in: $i"
+        sleep 1
+done
+echo "shutdown -r now"
+
+
---
+++ SETUP.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo Installing Git...
+sudo apt-get install git-all -yqq
+echo Repository cloning...
+cd /usr/local/bin
+sudo git clone https://github.com/GraveFruit/SoundSphere.git
+echo Installing SoundSphere...
+cd SoundSphere
+sudo sh INSTALL.sh
